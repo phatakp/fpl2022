24c92da67e8e4a1d2b3bef4915b39d97ed24c6ef
Praveen Phatak <praveenphatak@gmail.com>
2022-03-10 16:43:09 +0530

    urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -13,6 +13,5 @@ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
-    re_path('.*', TemplateView.as_view(template_name='index.html',
-            content_type='text/html')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
